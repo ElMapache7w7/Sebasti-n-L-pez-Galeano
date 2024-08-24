c2927452aed24207f13e6ac2ddc2ab51aa24a237
ElMapache7w7 <lopezjuansebastian1982@gmail.com>
2024-08-24 09:36:03 -0500

    Add files via upload

---
+++ 3 numeros multiplicados.py
@@ -0,0 +1,10 @@
+#multiplicar 3 números
+def multiplicar (a,b,c):
+    return a*b*c
+numero1 = 2
+numero2 = 3
+numero3 = 4
+
+resultado = multiplicar (numero1,numero2,numero3)
+
+print ("Su resultado es", resultado)
---
+++ area de un cuadrado.py
@@ -0,0 +1,11 @@
+# Solicitar al usuario que ingrese la longitud del lado del cuadrado
+
+lado = float(input("ingrese la longitud del cuadrado:"))
+
+# Calcular el área del cuadrado
+
+area = lado ** 2
+
+# Mostrar el resultado
+
+print (f"El área del cuadrado es: {area}")

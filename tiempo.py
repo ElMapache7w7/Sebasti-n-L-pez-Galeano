79d0ceb5ae53c46e8c3728262e51e8b71ed92177
ElMapache7w7 <lopezjuansebastian1982@gmail.com>
2024-09-10 13:27:15 -0500

    Delete tiempo.py

--- tiempo.py
+++
@@ -1,9 +0,0 @@
-#Solicitar tiempo en segundos y transformar a horas y minutos.
-#pedir segundos
-segundos = int(input("Ingrese el tiempo en segundos: "))
-#transformar a horas
-horas = segundos // 3600
-#transformar a minutos
-minutos = (segundos % 3600) // 60
-#imprimir
-print (f"El tiempo en horas es: {horas} horas y {minutos} minutos")

5566990ea36c92d126fe276344c3906aaba6782d
ElMapache7w7 <lopezjuansebastian1982@gmail.com>
2024-09-10 13:21:18 -0500

    Delete 20% de un numero.py

--- 20% de un numero.py
+++
@@ -1,9 +0,0 @@
-#solicitar al usuario un numero
-
-numero = float (input("ingrese un numero: "))
-
-#calcular el 20% del numero
-
-porcentaje = numero * 0.20
-
-print (f"el 20% de {numero} es {porcentaje}")

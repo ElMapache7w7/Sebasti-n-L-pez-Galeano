133022eaa2407c8d3c02aea257264d64ce0b05ab
ElMapache7w7 <lopezjuansebastian1982@gmail.com>
2024-09-10 13:31:39 -0500

    Delete punto 5.py

--- punto 5.py
+++
@@ -1,7 +0,0 @@
-#Escriba un programa para mostrar la tabla de multiplicar de un entero dado
-#Entrada: un entero
-#Salida: la tabla de multiplicar del entero dado
-n = int(input("Ingrese un numero: "))
-for i in range(1,11):
-  print(n,"x",i,"=",n*i)
-  

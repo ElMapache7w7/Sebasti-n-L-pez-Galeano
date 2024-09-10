5dc2d8392d19b75b2d0433a29792729e501a1c49
ElMapache7w7 <lopezjuansebastian1982@gmail.com>
2024-09-10 13:54:29 -0500

    Delete raiz cuadrada.py

--- raiz cuadrada.py
+++
@@ -1,9 +0,0 @@
-#Calcular la hipotenusa con el Teorema de Pitágoras (sin usar funciones).
-#solicitar cateto 1
-cateto1 = int("Ingrese el cateto 1")
-#solicitar cateto 2
-cateto2 = int("Ingrese el cateto 2")
-#calcular hipotenusa
-hipotenusa = (cateto1 ** 2 + cateto2 ** 2) ** 2
-#imprimir resultado
-print(f"La hipotenusa es {hipotenusa}:")

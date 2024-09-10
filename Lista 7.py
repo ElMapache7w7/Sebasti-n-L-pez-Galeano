67f9b87e790d938c246f27d5a11863fd2f793802
ElMapache7w7 <lopezjuansebastian1982@gmail.com>
2024-09-10 13:08:27 -0500

    Delete Lista 7.py

--- Lista 7.py
+++
@@ -1,6 +0,0 @@
-#Eliminar cadenas vacías de la lista de cadenas. Ejemplo dado: list1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]Resultado esperado: ["Mike", "Emma", "Kelly", "Brad"]
-lista1 = ["Mike", "", "Emma", "Kelly", "", "Brad"]
-print(lista1)
-lista1.remove("")
-lista1.remove("")
-print(lista1)
